a598d5674c1eb357bfb0adfb8e78a91632f652c4
John Hauff <jthauff@gmail.com>
2021-12-02 01:43:41 -0500

    Added to and cleaned up features section

--- components/Feature.js
+++ components/Feature.js
@@ -72,21 +72,27 @@ const Feature = () => {
       desc: "The Smart Bird Feeder will detect when an invading squirrel is nearby and will activate its defense system: cutting the squirrel off from the bird feed and playing a sound to deter them even further.",
     },
     {
-      id: 4,
+      id: 2,
       img: "https://i.imgur.com/lUi0xNJ.gif",
       title: "Easily Refill Your Feeder",
       desc: "The Smart Bird Feeder is equipped with an easy-to-use refill spout, complete with a cute bird ornament, which allows you to refill your feeder without the need to get inside of it.",
     },
     {
       id: 3,
-      img: "https://i.imgur.com/Fc8g71a.gif",
+      img: "https://i.imgur.com/1iLpSSV.gif",
+      title: "Portable And Rechargeable, With A Battery That Lasts",
+      desc: "The Smart Bird Feeder is portable and includes a rechargeable battery that will last hours. This is largely possible due to the power-efficient motion sensor built into The Smart Bird Feeder, which allows the feeder to use less power when there is nothing nearby.",
+    },
+    {
+      id: 4,
+      img: "https://i.imgur.com/Tz9XItZ.gif",
       title: "Stay Notified of Bird Activity and Never Miss a Moment",
       desc: "The Smart Bird Feeder will immediately notify you when it captures a new memory of a bird that's feeding from it. It's also able to detect the species of the bird and conveniently display it for you in a push notification and in the beautiful Bird Memories Gallery on our mobile app. This means you'll never need to wait around watching for birds, and instead you can view high quality images giving the best up-close views of the birds on your phone, or spring into action and go see the birds in-person whenever you get a notification.",
     },
     {
-      id: 4,
+      id: 5,
       img: "https://i.imgur.com/LP6SytZ.gif",
-      title: "You'll Know When The Birds Are Hungry",
+      title: "You'll Know When The Birds Are Getting Hungry",
       desc: "The Smart Bird Feeder will also automatically notify you when it is running out of bird feed by pushing a notification to your phone. This means you'll know exactly when to refill your feeder, without having to constantly check it yourself.",
     },
   ];
